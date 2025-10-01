15e49b971c270983c3415a24eaf02f37bba1c407
xexoxa <Ignatchik_MA_25@student.grsu.by>
2025-10-01 19:44:47 +0300

    Исправлен текст результата

--- Lab_2/A4.py
+++ Lab_2/A4.py
@@ -1,4 +1,4 @@
 A = int (input ("Введите число A от 1 до 1000: "))
 B = int (input("Введите число B от 1 до 1000: "))
-result = "Число А больше чем В" if A > B else "Число В больше чем А"
+result = A if A > B else B
 print(result)
