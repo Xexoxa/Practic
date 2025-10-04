b2e113fcc2ec52cb530a00dff8e95fdf51ac80cb
Xexoxa <Ignatchik_MA_25@student.grsu.by>
2025-10-04 16:41:36 +0300

    Вывод ответа

--- Lab_2/A7.py
+++ Lab_2/A7.py
@@ -4,4 +4,6 @@ b1 = int(input())
 b2 = int(input())
 a3 = ["White", "Black"][(a1-a2)%2==1]
 b3 = ["White", "Black"][(b1-b2)%2==1]
-print(a3,b3)
+n = ["NO","YES"][a3==b3]
+print(n)
+print(a3) if n=="YES" else print()
