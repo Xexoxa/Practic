6791e75f417fa09339f39a5e6b758a166a144ec7
Xexoxa <Ignatchik_MA_25@student.grsu.by>
2025-10-14 11:53:44 +0300

    Добавлена проверка на длину, строчные, заглавные и цифры

--- Lab_3/A2.py
+++ Lab_3/A2.py
@@ -1,2 +1,11 @@
 import string
 p=input()
+n=0
+if len(p)!=8: print("Длина пароля не равна 8")
+else: n+=1
+if p.lower()==p: print("В пароле отсутствуют заглавные буквы")
+else: n+=1
+if p.upper()==p: print("В пароле отсутствуют строчные буквы")
+else: n+=1
+if (any(symbol.isdigit() for symbol in p))=="False": print("В пароле отсутствуют цифры")
+else: n+=1
