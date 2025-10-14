b4a5c7f17859b93ecc4cd89966a7f54bf80c78dc
Xexoxa <Ignatchik_MA_25@student.grsu.by>
2025-10-14 10:11:35 +0300

    Ввод координат

---
+++ Lab_3/A1.py
@@ -0,0 +1,15 @@
+x1 = int (input())
+y1 = int (input())
+x2 = int (input())
+y2 = int (input())
+if (x1>0 and y1>0):
+ n=1
+elif (x1<0 and y1>0):
+ n=2
+elif (x1<0 and y1<0):
+ n=3
+elif (x1>0 and y1<0):
+ n=4
+else:
+ n=0
+ 
