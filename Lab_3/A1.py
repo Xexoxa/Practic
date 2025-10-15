2e02ff7b74fe950b9e3193594a975e67bd211b15
Xexoxa <Ignatchik_MA_25@student.grsu.by>
2025-10-15 09:47:23 +0300

    Исправлен внешний вид A1

--- Lab_3/A1.py
+++ Lab_3/A1.py
@@ -2,30 +2,19 @@ x1 = int (input())
 y1 = int (input())
 x2 = int (input())
 y2 = int (input())
-if (x1>0 and y1>0):
- n="I"
-elif (x1<0 and y1>0):
- n="II"
-elif (x1<0 and y1<0):
- n="III"
-elif (x1>0 and y1<0):
- n="IV"
-else:
- n=0
-if (x2>0 and y2>0):
- m="I"
-elif (x2<0 and y2>0):
- m="II"
-elif (x2<0 and y2<0):
- m="III"
-elif (x2>0 and y2<0):
- m="IV"
-else:
- m=0
-if (n==0 or m==0):
- print("Ошибка")
-elif (n==m):
- print("YES, ", n)
-else:
- print("NO")
- 
+
+if (x1>0 and y1>0):n="I"
+elif (x1<0 and y1>0):n="II"
+elif (x1<0 and y1<0):n="III"
+elif (x1>0 and y1<0):n="IV"
+else:n=0
+
+if (x2>0 and y2>0):m="I"
+elif (x2<0 and y2>0):m="II"
+elif (x2<0 and y2<0):m="III"
+elif (x2>0 and y2<0):m="IV"
+else:m=0
+
+if (n==0 or m==0):print("Ошибка")
+elif (n==m):print("YES, ", n)
+else:print("NO")
