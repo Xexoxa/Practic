fa003b41e864bf6822dcb1979d86374e4332a5aa
Xexoxa <Ignatchik_MA_25@student.grsu.by>
2025-10-21 12:24:20 +0300

    Вывод правого треугольника

--- Lab_4/A2.py
+++ Lab_4/A2.py
@@ -2,8 +2,15 @@ n=int(input())
 m=int(input())
 print("ПРЯМОУГОЛЬНИК",n, "x",m)
 for i in range(n): 
-    
     for j in range(m): 
         print('#', end='')
     print()
+print()
+print("ПРАВЫЙ ТРЕУГОЛЬНИК:")
+o=1
+for i in range(n):
+    print(o*'#', end='')
+    o+=1
+    print()
+    
 
