11aafe10a3b16aa6c35e30826ce86e7a77f14d41
Xexoxa <Ignatchik_MA_25@student.grsu.by>
2025-11-05 11:01:03 +0300

    Удаление скобок и вывод ответа

--- Lab_5/A1.py
+++ Lab_5/A1.py
@@ -1,3 +1,6 @@
 t=input("Исходный текст: ")
-l= t.find('(')
-r= t.find(')')
+while '(' in t or ')' in t:
+    l= t.rfind('(')
+    r= t.find(')',l)
+    t = t.replace(t[l:r + 1], '')
+print("Укороченный текст: ",t)
