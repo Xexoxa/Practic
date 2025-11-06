007fa4a77859f1a9142484e11c7331e7ee325dfc
Xexoxa <Ignatchik_MA_25@student.grsu.by>
2025-11-06 12:43:25 +0300

    Открытие определенной строки файла

--- Lab_5/A4.py
+++ Lab_5/A4.py
@@ -1,8 +1,12 @@
 
-def file_open(file):
+def file_open(file, row):
     xfile = open(file, 'r')
+    i=0
     for s in xfile:
         n=s.split('\t')
-        print(n)
-print(file_open('sequences.0.txt'))
+        if i==row:
+            break
+        i+=1
+    return n
+print(file_open('sequences.0.txt', 1))
 
